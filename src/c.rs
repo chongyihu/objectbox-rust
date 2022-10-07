532e59b698148510088c9b3e5870bca3edb5665f
Jasm Sison <jasm.sison@gmail.com>
2022-10-07 03:23:58 +0200

    allow unused variables, kill warnings in generated code

--- src/c.rs
+++ src/c.rs
@@ -2,6 +2,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(dead_code)]
 include!(concat!(env!("OUT_DIR"), "/objectbox-c-bindings.rs"));
 
 use crate::error::Error;
