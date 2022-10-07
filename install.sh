9298dbda9c00de232c23289946d5be90931700cd
Jasm Sison <jasm.sison@gmail.com>
2022-10-07 03:23:33 +0200

    update objectbox binary

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-cLibVersion=0.7.1
+cLibVersion=0.11.0
 os=$(uname)
 
 # if there's no tty this is probably part of a docker build - therefore we install the c-api explicitly
