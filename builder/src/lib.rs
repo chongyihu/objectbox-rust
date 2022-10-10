97a49591254091f04d3ecd6ab540dbef2084ed28
Jasm Sison <jasm.sison@gmail.com>
2022-10-10 17:21:50 +0200

    fleshed out how to parse attribute parameters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,8 +7,9 @@ extern crate prettyplease;
 
 use proc_macro::TokenStream;
 
+use quote::ToTokens;
 use syn::punctuated::Pair;
-use syn::NestedMeta::Meta;
+use syn::NestedMeta::{Meta, self};
 use syn::Meta::NameValue;
 use syn::Data::Struct;
 use syn::Lit::Int;
@@ -34,84 +35,7 @@ const OBXPropertyType_Flex: u16 = 13; // tuple?! There is no 'Object' in rustlan
 const OBXPropertyType_ByteVector: u16 = 23; // Vec<u8>, bytes, ByteArray, unsized byte slice, compile time statically sized array on stack
 const OBXPropertyType_StringVector: u16 = 30; // Vec<str> / Vec<String>
 
-// TODO see if uid type = u64 can be parameterized with generics e.g. 0x... 0b... etc.
-// TODO see quote::format_ident
-struct Field {
-  name: String,
-  field_type: u16,
-  ///
-  id: Option<u64>, // Do we do that id_uid thing?
-  uid: Option<u64>,
-  unique: bool
-}
-
-impl Field {
-  fn from_syn_field(field: &syn::Field) -> Option<Field> {
-    // TODO check if objectbox-model.json was generated
-    // TODO compare values read from macro attributes
-    
-    if let Some(ident) = &field.ident {
-
-      let field_type = match &field.ty {
-        syn::Type::Array(type_array) => {
-          // TODO
-          0
-        },
-        // BareFn(TypeBareFn),
-        syn::Type::Group(type_group) => {
-          // TODO
-          0
-        },
-        // ImplTrait(TypeImplTrait),
-        // Infer(TypeInfer),
-        // Macro(TypeMacro),
-        // Never(TypeNever),
-        // Paren(TypeParen),
-        // Path(TypePath),
-        // Ptr(typePtr)
-        // Reference(typeReference)
-        syn::Type::Slice(type_slice) => {
-          // TODO
-          0
-        },
-        // TraitObject(TypeTraitObject), 
-        syn::Type::Tuple(type_tuple) => {
-          // TODO
-          0
-        }, // Flex?
-        syn::Type::Verbatim(token_stream) => {
-          // TODO
-          0
-        }
-        _ => {
-          0
-        }
-      };
-
-      return Some(Field {
-        name: ident.to_string(),
-        field_type: field_type,
-        id: None, // TODO
-        uid: None, // TODO
-        unique: false // TODO
-      });
-    }
-
-    Option::None
-  }
-}
-
-// TODO Make this JSON serializable
-// TODO see if uid type = u64 can be parameterized with generics e.g. 0x... 0b... etc.
-// TODO see how generics work with this e.g. "struct Gen<T> { field: T }"
-// TODO see how fields with Option<T> type, that default to None, and how store deals with this
-// TODO check if another attribute macro can mess with our attribute, otherwise panic if another attribute is present
-struct Entity {
-  name: String,
-  uid: Option<u64>,
-  fields: Vec<Field>
-}
-
+/// For lack of the Debug trait in certain tokens
 /// Only available in Debug mode
 fn print_token_stream(label: &str, stream: TokenStream) {
   if cfg!(debug_assertions) {
@@ -123,23 +47,182 @@ fn print_token_stream(label: &str, stream: TokenStream) {
   }
 }  
 
+// For lack of the Debug trait in certain tokens
 // TODO Figure out how to make this generic, since TokenStream2 inherits from TokenStream
 // TODO maybe a simple 'x as proc_macro::TokenStream' might suffice
-// fn print_token_stream2(label: &str, stream: proc_macro2::TokenStream) {
-//   if cfg!(debug_assertions) {
-//     stream.into_iter()
-//       .for_each(|x| {
-//         println!("{} {:#?}", label, x);
-//       });
-//     println!("{}", "///");
-//   }
-// }  
+fn print_token_stream2(label: &str, stream: proc_macro2::TokenStream) {
+  if cfg!(debug_assertions) {
+    stream.into_iter()
+      .for_each(|x| {
+        println!("{} {:#?}", label, x);
+      });
+    println!("{}", "///");
+  }
+}  
+
+// TODO see if uid type = u64 can be parameterized with generics e.g. 0x... 0b... etc.
+// TODO see quote::format_ident
+// TODO replace with serde
+struct Field {
+  name: String,
+  field_type: u16,
+  ///
+  id: Option<u64>, // Do we do that id_uid thing?
+  uid: Option<u64>,
+  unique: bool
+  // TODO all the attributes
+}
+
+impl Field {
+  fn from_syn_field(field: &syn::Field) -> Option<Field> {
+    // TODO check if objectbox-model.json was generated
+    // TODO compare values read from macro attributes
+
+    let mut entity = 
+      Field {
+        name: String::new(),
+        field_type: 0,
+        id: None,
+        uid: None,
+        unique: false
+    };
+
+    if let Some(ident) = &field.ident {
+      let new_name = ident.to_string();
+      entity.name.push_str(&new_name);
+
+      for a in field.attrs.iter() {
+        // get attribute name from `#[name]`
+        if let Some(attr_path_ident) = a.path.get_ident() {
+          let attr_name : &str = &attr_path_ident.to_string();
+          match attr_name {
+            "index" => {},
+            "unique" => {},
+            "backlink" => {},
+            "transient" => {},
+            "property" => {},
+            _ => {}
+          }  
+        }
+        // I don't know why we need this yet
+        // if let syn::parse::Result::Ok(m) = a.parse_meta() {
+          /*
+          enum Meta {
+            Path(Path),
+            List(MetaList),
+            NameValue(MetaNameValue),
+          }
+          */
+        // }
+        let args_result = a.parse_args::<NestedMeta>();
+        if let syn::parse::Result::Ok(args) = args_result {
+          // TODO maybe Meta is a better choice, we'll see
+          // TODO for multiple params you probably need to parse Punctuated NameValues
+          // TODO maybe parsing the TokenStream is easier
+        }
+      }
+      
+
+      let field_ts = field.to_token_stream();
+      let  debug_label = format!("field({}) token_stream", ident.to_string());
+      print_token_stream2(&debug_label, field_ts);
+      
+      // mental note: it's probably easier to parse the token stream,
+      // than exhaustively go through these matches
+      // This is here to figure out how different types are parsed (then call get_ident or something)
+      let field_type = match &field.ty {
+        syn::Type::Array(type_array) => {
+          // TODO
+          // println!("{} {:#?}", ident.to_string(), type_array);
+          /*
+          struct TypeArray {
+            bracket_token: Bracket,
+            elem: Box<Type>,
+            semi_token: Semi,
+            len: Expr,
+          }
+          */
+          // For lack of the Debug trait in certain tokens
+          println!("{} {}", debug_label, "Array");
+          0
+        },
+        // BareFn(TypeBareFn),
+        syn::Type::Group(type_group) => {
+          // TODO
+          /*
+          struct TypeGroup {
+            group_token: Group,
+            elem: Box<Type>,
+          }
+          */
+          // For lack of the Debug trait in certain tokens
+          println!("{} {}", debug_label, "Group");
+          0
+        },
+        // ImplTrait(TypeImplTrait),
+        // Infer(TypeInfer),
+        syn::Type::Macro(type_macro) => {
+          println!("{} {}", debug_label, "Macro");
+          0
+        },
+        // Never(TypeNever),
+        // Paren(TypeParen),
+        syn::Type::Path(type_path) => {
+          println!("{} {}", debug_label, "Path");
+          0
+        },
+        // Ptr(typePtr)
+        // Reference(typeReference)
+        syn::Type::Slice(type_slice) => {
+          // TODO
+          // For lack of the Debug trait in certain tokens
+          println!("{} {}", debug_label, "Slice");
+          0
+        },
+        // TraitObject(TypeTraitObject), 
+        syn::Type::Tuple(type_tuple) => {
+          // TODO
+          // For lack of the Debug trait in certain tokens
+          println!("{} {}", debug_label, "Tuple");          
+          0
+        }, // Flex?
+        syn::Type::Verbatim(token_stream) => {
+          // TODO
+          // For lack of the Debug trait in certain tokens
+          println!("{} {}", debug_label, "Verbatim");
+          0
+        }
+        _ => {
+          0
+        }
+      };
+
+      return Some(entity);
+    }
+
+    Option::None
+  }
+}
+
+// TODO Make this JSON serializable, or another like it, with an adapter 'from' in-between
+// TODO see if uid type = u64 can be parameterized with generics e.g. 0x... 0b... etc.
+// TODO see how generics work with this e.g. "struct Gen<T> { field: T }"
+// TODO see how fields with Option<T> type, that default to None, and how store deals with this
+// TODO check if another attribute macro can mess with our attribute, otherwise panic if another attribute is present
+// TODO replace with serde
+struct Entity {
+  name: String,
+  id: Option<u64>,
+  uid: Option<u64>,
+  fields: Vec<Field>
+}
 
 impl Entity {
   /// Unnamed fields are ignored, e.g. nested anonymous unions / structs, like in C.
   fn parse_entity_name_and_fields(derive_input: DeriveInput) -> Entity {
     let mut entity = Entity {
       name: derive_input.ident.to_string(),
+      id: Option::None,
       uid: Option::None,
       fields: Vec::new()
     };
@@ -174,28 +257,27 @@ impl Entity {
   // TODO this can probably be reused to read the field attributes
   // TODO pass a lambda to the extracted function
   fn parse_entity_attribute_parameter(mut self, args: AttributeArgs) {
-    fn panic_only_one_param_allowed() {
+    fn panic_only_id_uid_param_allowed() {
       panic!("Only the uid=<integer> parameter is allowed");
     }
 
-    if args.len() > 1 {
-      panic_only_one_param_allowed()
+    if args.len() > 2 {
+      panic_only_id_uid_param_allowed()
     }
 
+    // TODO parse id and uid
     if let Some(Meta(NameValue(mnv))) = args.iter().next() {
-      if !mnv.path.is_ident("uid") {
-        panic_only_one_param_allowed();
-      }
+
 
       if let Int(li) = &mnv.lit {
         let result = li.base10_parse::<u64>();
         if let Ok(value) = result {
-          self.uid = Some(value);
+          // self.uid = Some(value);
           return;
         }
       }
 
-      panic_only_one_param_allowed();
+      panic_only_id_uid_param_allowed();
     }
   }
 }
@@ -208,7 +290,7 @@ impl Entity {
 // TODO also remove those imports
 #[proc_macro_attribute]
 pub fn entity(args: TokenStream, input: TokenStream) -> TokenStream {
-  print_token_stream("all: ", input.clone());
+  // print_token_stream("all: ", input.clone());
 
   let struct_clone = input.clone();
   let struct_info = parse_macro_input!(struct_clone as DeriveInput);
--- tests/builder.rs
+++ tests/builder.rs
@@ -1,25 +1,27 @@
 use objectbox;
 extern crate builder;
 
-use builder::{entity, index, transient, unique};
+use builder::{entity, index, transient, unique, sync};
 
 // #[entity]
 // struct SomeEmptyType; // expected: ignore completely
 
-#[entity]
-struct CorrectType {
+#[sync]
+#[entity(id=1, uid=3)]
+struct Type_Test {
   t_bool : bool,
-  #[unique(uid=1339)]
+  #[unique(id=1,uid=1339)]
   t_u32 : u32,
 }
 
 #[entity(uid=1337)]
-struct LessEmptyTypeWithParams {
+struct Type_Test_Again {
   #[index(uid=1338)]
   t_bool : bool,
   #[transient]
   t_u32 : u32,
   t_u64 : u64,
+  #[property(id=1333, uid=111111)]
   t_double : f64,
 }
 
@@ -39,6 +41,6 @@ struct LessEmptyTypeWithParams {
 
 #[test]
 fn test_entity_codegen() {
-  let an_entity = CorrectType { t_bool : true, t_u32 : 1337 };
+  let an_entity = Type_Test { t_bool : true, t_u32 : 1337 };
   assert_eq!(an_entity.t_u32, 1337);
 }
