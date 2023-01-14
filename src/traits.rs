cf6843299d889c87014d0ee4d818903374c84650
Jasm Sison <jasm.sison@gmail.com>
2023-01-14 17:41:39 +0100

    failed attempt at putting various factory closures with a different return type in a Map

--- src/traits.rs
+++ src/traits.rs
@@ -4,8 +4,6 @@ pub mod store;
 #[path = "./model.rs"]
 mod model;
 
-use bytebuffer::ByteBuffer;
-
 pub trait FBOBBridge {
   fn to_FB(self /* TODO, builder: &fb.Builder */);
 
@@ -36,55 +34,96 @@ trait OBBlanket: IdExt + FBOBBridge {}
 impl<T> OBBlanket for T where T: IdExt + FBOBBridge {}
 
 #[cfg(test)]
-  #[test]
-  fn tuck_blanket() {
-    // imagine this were an external struct
-    // from a different package / crate / module etc.
+#[test]
+fn tuck_blanket() {
+  // imagine this were an external struct
+  // from a different package / crate / module etc.
 
-    struct SomeEntity {
-      id: model::SchemaID
-    }
-
-    impl FBOBBridge for SomeEntity {
-      fn to_FB(self /* TODO, builder: &fb.Builder */) {}
-
-      // non-member method, static(?) factory function, can't dispatch on a trait
-      // fn from_FB(store: &mut store::Store, byte_buffer: &ByteBuffer) -> Self {
-      //   SomeEntity { id: 1 }
-      // }
-    }
-
-    impl IdExt for SomeEntity {
-      fn get_id(&self) -> model::SchemaID {
-        self.id
-      }
-      fn set_id(&mut self, id: model::SchemaID) {
-        self.id = id;
-      }
-    }
-
-    // call trait method on original object
-    let e0 = SomeEntity{ id: 1 };
-
-    assert_eq!(e0.get_id(), 1);
-
-    // single-owner boxed immutable
-    let b1 = Box::new(SomeEntity { id: 3});
-    let t1 = b1 as Box<dyn OBBlanket>;
-
-    assert_eq!(t1.get_id(), 3);
-
-    // borrowed mutable
-    let e2 = &mut SomeEntity{ id: 5 };
-    let m2 = e2 as &mut dyn OBBlanket;
-
-    m2.set_id(5005);
-
-    assert_eq!(m2.get_id(), 5005);
-
-    // borrowed immutable
-    let e3 = &SomeEntity{ id: 6 };
-    let r3 = e3 as &dyn OBBlanket;
-
-    assert_eq!(r3.get_id(), 6);
+  struct SomeEntity {
+    id: model::SchemaID
   }
+
+  impl FBOBBridge for SomeEntity {
+    fn to_FB(self /* TODO, builder: &fb.Builder */) {}
+
+    // non-member method, static(?) factory function, can't dispatch on a trait
+    // fn from_FB(store: &mut store::Store, byte_buffer: &ByteBuffer) -> Self {
+    //   SomeEntity { id: 1 }
+    // }
+  }
+
+  impl IdExt for SomeEntity {
+    fn get_id(&self) -> model::SchemaID {
+      self.id
+    }
+    fn set_id(&mut self, id: model::SchemaID) {
+      self.id = id;
+    }
+  }
+
+  // call trait method on original object
+  let e0 = SomeEntity{ id: 1 };
+
+  assert_eq!(e0.get_id(), 1);
+
+  // single-owner boxed immutable
+  let b1 = Box::new(SomeEntity { id: 3});
+  let t1 = b1 as Box<dyn OBBlanket>;
+
+  assert_eq!(t1.get_id(), 3);
+
+  // borrowed mutable
+  let e2 = &mut SomeEntity{ id: 5 };
+  let m2 = e2 as &mut dyn OBBlanket;
+
+  m2.set_id(5005);
+
+  assert_eq!(m2.get_id(), 5005);
+
+  // borrowed immutable
+  let e3 = &SomeEntity{ id: 6 };
+  let r3 = e3 as &dyn OBBlanket;
+
+  assert_eq!(r3.get_id(), 6);
+}
+
+
+use bytebuffer::ByteBuffer;
+
+struct Factory<T> {
+    pub builder: fn(store: &mut store::Store, byte_buffer: &ByteBuffer) -> T,
+}
+
+#[cfg(test)]
+#[test]
+fn build_factories() {
+  use std::collections::HashMap;
+  let mut map = HashMap::new();
+
+  let f1 = Factory {
+    builder: |s, bb| String::from("new"),
+  };
+
+  let f2 = Factory {
+    builder: |s, bb| 32_u32,
+  };
+
+  // map.insert(
+  //   0_u32,
+  //   Box::new(f1),
+  // );
+
+  map.insert(
+    1_u32,
+    Box::new(f2),
+  );
+
+  // let result0 = map.get(&0_u32).unwrap();
+  let result1 = map.get(&1_u32).unwrap();
+
+  let store = &mut store::Store {};
+  let bb = bytebuffer::ByteBuffer::new();
+
+  // assert_eq!((result0.builder)(store, &bb).as_str(), "new");
+  assert_eq!((result1.builder)(store, &bb), 32_u32);
+}
