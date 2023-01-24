c01258c06268c349e7a46139ffb90ed416483511
Jasm Sison <jasm.sison@gmail.com>
2023-01-24 17:29:21 +0100

    replaced copy/paste atrocity

--- src/entity_builder.rs
+++ src/entity_builder.rs
@@ -40,32 +40,17 @@ impl EntityBuilder {
   }
 
   pub(crate) fn add_property(&mut self, name: &str, id: SchemaID, uid: SchemaUID, typ: OBXPropertyType, flags: OBXPropertyFlags) -> &mut Self {
-      // let property = 
-      //   Property {
-      //     name: name.to_string(),
-      //     id,
-      //     uid,
-      //     typ,
-      //     flags
-      //   };
-      // On the OBX side of things, pretend char is u32
-      let property = if typ == c::OBXPropertyType_Char {
-        Property {
+      let (new_type, new_flags) = if typ == c::OBXPropertyType_Char {
+        (c::OBXPropertyType_Char, flags & c::OBXPropertyFlags_UNSIGNED)
+      }else {
+        (typ, flags)
+      };
+      let property = Property {
           name: name.to_string(),
           id,
           uid,
-          typ,
-          flags
-        }
-      } else {
-        // don't actually use char on ob
-        Property {
-          name: name.to_string(),
-          id,
-          uid,
-          typ: c::OBXPropertyType_Int,
-          flags: flags | c::OBXOrderFlags_UNSIGNED
-        }
+          typ: new_type,
+          flags: new_flags,
       };
       let last = &mut self.entities.last_mut();
       match last {
