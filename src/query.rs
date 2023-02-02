11e7c9728d64ff9f81b3665f2b330d55f5091d80
Jasm Sison <jasm.sison@gmail.com>
2023-02-02 06:35:59 +0100

    changed comment style

--- src/query.rs
+++ src/query.rs
@@ -1,17 +1,17 @@
-/// TODO implement Drop on QueryProperty etc.
-/// TODO whichever holds a C ptr, that has a free/close C fn
-/// 
-/// Reminder:
-/// Expression in dart: box.query(i.greaterThan(0)).build().property(pq);
-/// box.query -> QueryBuilder
-/// i -> QueryProperty (QP)
-/// i.greaterThan(0) -> Condition
-/// ..build() -> Query
-/// ..property(j) -> PropertyQuery (PQ) PQ vs QP are confusing as hell, I named it, mea culpa
-/// j -> QP (like i)
-/// 
-/// Traits to reuse: https://doc.rust-lang.org/std/ops/index.html
-/// Ops: https://doc.rust-lang.org/book/appendix-02-operators.html
+// TODO implement Drop on QueryProperty etc.
+// TODO whichever holds a C ptr, that has a free/close C fn
+// 
+// Reminder:
+// Expression in dart: box.query(i.greaterThan(0)).build().property(pq);
+// box.query -> QueryBuilder
+// i -> QueryProperty (QP)
+// i.greaterThan(0) -> Condition
+// ..build() -> Query
+// ..property(j) -> PropertyQuery (PQ) PQ vs QP are confusing as hell, I named it, mea culpa
+// j -> QP (like i)
+// 
+// Traits to reuse: https://doc.rust-lang.org/std/ops/index.html
+// Ops: https://doc.rust-lang.org/book/appendix-02-operators.html
 /*
 enum _ConditionOp {
   isNull, // TODO only feasible when Option<OB_Rust_Primitive> is introduced
