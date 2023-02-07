8f706768471c4260f307431831678c1fc9beccb5
Jasm Sison <jasm.sison@gmail.com>
2023-02-07 18:43:18 +0100

    prepare fns for macro transformation

---
+++ src/query/builder.rs
@@ -0,0 +1,508 @@
+// TODO write macro for boilerplate: fn obx_query_something(...) -> obx_err, rewrite to get_result,
+// TODO use rusty result operators (or, or_else, ? etc.) to chain results
+// TODO also error check before chaining the next call (obx_qb_cond)
+// TODO depending on property type, allow only certain calls at compile time?
+// TODO compile time determined extension blanket traits?
+// TODO also use traits for operator overloading on specific functions (can't use blankets tho)
+use crate::{
+    c::*,
+    error,
+    store::Store,
+    traits::FactoryHelper, util::PtrConstChar,
+};
+
+use std::rc::Rc;
+use super::query::Query;
+
+impl Drop for Builder {
+    fn drop(&mut self) {
+        if !self.has_built_query && !self.obx_query_builder.is_null() {
+            self.close();
+            self.obx_query_builder = std::ptr::null_mut();
+        }
+
+        if let Some(err) = &self.error {
+            eprintln!("Error: async: {err}");
+        }
+    }
+}
+
+pub struct Builder {
+    error: Option<error::Error>,
+    pub(crate) obx_query_builder: *mut OBX_query_builder,
+    // TODO refresh memory: when transformed to query it's closed?
+    // TODO so set flag when passing this object to a Query
+    // builder.do_stuff().build() <- here to prevent drop (double free)
+    has_built_query: bool,
+}
+
+impl Builder {
+    pub fn from_store_and_entity_id<T>(store: &Store, factory: Rc<dyn FactoryHelper<T>>) -> Self {
+        let entity_id = factory.get_entity_id(); // call factory
+        let obx_query_builder = unsafe { obx_query_builder(store.obx_store, entity_id) };
+        Builder {
+            obx_query_builder,
+            error: None,
+            has_built_query: false,
+        }
+    }
+
+    pub fn build(&mut self) -> error::Result<Query> {
+        if let Some(err) = &self.error {
+            Err(err.clone())
+        } else {
+            self.has_built_query = true;
+            Ok(Query::from_query_builder(self.obx_query_builder))
+        }
+    }
+
+    /// private, in case of double frees
+    fn close(&mut self) -> obx_err {
+        unsafe { obx_qb_close(self.obx_query_builder) }
+    }
+
+    pub(crate) fn type_id(&self) -> obx_schema_id {
+        unsafe { obx_qb_type_id(self.obx_query_builder) }
+    }
+
+    fn error_code(&self) -> obx_err {
+        unsafe { obx_qb_error_code(self.obx_query_builder) }
+    }
+
+    fn error_message(&self) -> PtrConstChar {
+        unsafe { obx_qb_error_message(self.obx_query_builder) }
+    }
+
+    pub(crate) unsafe fn is_null(&mut self, property_id: obx_schema_id) -> obx_qb_cond {
+        obx_qb_null(self.obx_query_builder, property_id)
+    }
+
+    pub(crate) unsafe fn not_null(&mut self, property_id: obx_schema_id) -> obx_qb_cond {
+        obx_qb_not_null(self.obx_query_builder, property_id)
+    }
+
+    pub(crate) unsafe fn equals_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        obx_qb_equals_string(self.obx_query_builder, property_id, value, case_sensitive)
+    }
+
+    pub(crate) unsafe fn not_equals_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_not_equals_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn contains_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_contains_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn contains_element_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_contains_element_string(
+                self.obx_query_builder,
+                property_id,
+                value,
+                case_sensitive,
+            )
+        }
+    }
+
+    pub(crate) unsafe fn contains_key_value_string(
+        &mut self,
+        property_id: obx_schema_id,
+        key: PtrConstChar,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_contains_key_value_string(
+                self.obx_query_builder,
+                property_id,
+                key,
+                value,
+                case_sensitive,
+            )
+        }
+    }
+
+    pub(crate) unsafe fn starts_with_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_starts_with_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn ends_with_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_ends_with_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn greater_than_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_greater_than_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn greater_or_equal_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_greater_or_equal_string(
+                self.obx_query_builder,
+                property_id,
+                value,
+                case_sensitive,
+            )
+        }
+    }
+
+    pub(crate) unsafe fn less_than_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_less_than_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn less_or_equal_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_less_or_equal_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn in_strings(
+        &mut self,
+        property_id: obx_schema_id,
+        values: *const PtrConstChar,
+        count: usize,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_in_strings(
+                self.obx_query_builder,
+                property_id,
+                values,
+                count,
+                case_sensitive,
+            )
+        }
+    }
+
+    pub(crate) unsafe fn any_equals_string(
+        &mut self,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        case_sensitive: bool,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_any_equals_string(self.obx_query_builder, property_id, value, case_sensitive)
+        }
+    }
+
+    pub(crate) unsafe fn equals_int(
+        &mut self,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_qb_cond {
+        obx_qb_equals_int(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn not_equals_int(
+        &mut self,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_qb_cond {
+        obx_qb_not_equals_int(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn greater_than_int(
+        &mut self,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_qb_cond {
+        obx_qb_greater_than_int(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn greater_or_equal_int(
+        &mut self,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_qb_cond {
+        obx_qb_greater_or_equal_int(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn less_than_int(
+        &mut self,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_qb_cond {
+        obx_qb_less_than_int(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn less_or_equal_int(
+        &mut self,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_qb_cond {
+        obx_qb_less_or_equal_int(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn between_2ints(
+        &mut self,
+        property_id: obx_schema_id,
+        value_a: i64,
+        value_b: i64,
+    ) -> obx_qb_cond {
+        obx_qb_between_2ints(self.obx_query_builder, property_id, value_a, value_b)
+    }
+
+    pub(crate) unsafe fn in_int64s(
+        &mut self,
+        property_id: obx_schema_id,
+        values: *const i64,
+        count: usize,
+    ) -> obx_qb_cond {
+        obx_qb_in_int64s(self.obx_query_builder, property_id, values, count)
+    }
+
+    pub(crate) unsafe fn not_in_int64s(
+        &mut self,
+        property_id: obx_schema_id,
+        values: *const i64,
+        count: usize,
+    ) -> obx_qb_cond {
+        obx_qb_not_in_int64s(self.obx_query_builder, property_id, values, count)
+    }
+
+    pub(crate) unsafe fn in_int32s(
+        &mut self,
+        property_id: obx_schema_id,
+        values: *const i32,
+        count: usize,
+    ) -> obx_qb_cond {
+        obx_qb_in_int32s(self.obx_query_builder, property_id, values, count)
+    }
+
+    pub(crate) unsafe fn not_in_int32s(
+        &self,
+        property_id: obx_schema_id,
+        values: *const i32,
+        count: usize,
+    ) -> obx_qb_cond {
+        obx_qb_not_in_int32s(self.obx_query_builder, property_id, values, count)
+    }
+
+    pub(crate) unsafe fn greater_than_double(
+        &self,
+        property_id: obx_schema_id,
+        value: f64,
+    ) -> obx_qb_cond {
+        obx_qb_greater_than_double(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn greater_or_equal_double(
+        &self,
+        property_id: obx_schema_id,
+        value: f64,
+    ) -> obx_qb_cond {
+        obx_qb_greater_or_equal_double(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn less_than_double(
+        &self,
+        property_id: obx_schema_id,
+        value: f64,
+    ) -> obx_qb_cond {
+        obx_qb_less_than_double(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn less_or_equal_double(
+        &self,
+        property_id: obx_schema_id,
+        value: f64,
+    ) -> obx_qb_cond {
+        obx_qb_less_or_equal_double(self.obx_query_builder, property_id, value)
+    }
+
+    pub(crate) unsafe fn between_2doubles(
+        &self,
+        property_id: obx_schema_id,
+        value_a: f64,
+        value_b: f64,
+    ) -> obx_qb_cond {
+        obx_qb_between_2doubles(self.obx_query_builder, property_id, value_a, value_b)
+    }
+
+    pub(crate) unsafe fn equals_bytes(
+        &self,
+        property_id: obx_schema_id,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_qb_cond {
+        obx_qb_equals_bytes(self.obx_query_builder, property_id, value, size)
+    }
+
+    pub(crate) unsafe fn greater_than_bytes(
+        &self,
+        property_id: obx_schema_id,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_qb_cond {
+        obx_qb_greater_than_bytes(self.obx_query_builder, property_id, value, size)
+    }
+
+    pub(crate) unsafe fn greater_or_equal_bytes(
+        &self,
+        property_id: obx_schema_id,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_qb_cond {
+        obx_qb_greater_or_equal_bytes(self.obx_query_builder, property_id, value, size)
+    }
+
+    pub(crate) unsafe fn less_than_bytes(
+        &self,
+        property_id: obx_schema_id,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_qb_cond {
+        obx_qb_less_than_bytes(self.obx_query_builder, property_id, value, size)
+    }
+
+    pub(crate) unsafe fn less_or_equal_bytes(
+        &self,
+        property_id: obx_schema_id,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_qb_cond {
+        obx_qb_less_or_equal_bytes(self.obx_query_builder, property_id, value, size)
+    }
+
+    pub(crate) unsafe fn relation_count_property(
+        &self,
+        relation_entity_id: obx_schema_id,
+        relation_property_id: obx_schema_id,
+        relation_count: i32,
+    ) -> obx_qb_cond {
+        unsafe {
+            obx_qb_relation_count_property(
+                self.obx_query_builder,
+                relation_entity_id,
+                relation_property_id,
+                relation_count,
+            )
+        }
+    }
+
+    // TODO create all!() macro, substitute varargs
+    pub(crate) unsafe fn all(&self, conditions: *const obx_qb_cond, count: usize) -> obx_qb_cond {
+        obx_qb_all(self.obx_query_builder, conditions, count)
+    }
+
+    // TODO create any!() macro, substitute varargs
+    pub(crate) unsafe fn any(&self, conditions: *const obx_qb_cond, count: usize) -> obx_qb_cond {
+        obx_qb_any(self.obx_query_builder, conditions, count)
+    }
+
+    pub(crate) unsafe fn param_alias(&self, alias: PtrConstChar) -> obx_err {
+        obx_qb_param_alias(self.obx_query_builder, alias)
+    }
+
+    pub(crate) unsafe fn order(&self, property_id: obx_schema_id, flags: OBXOrderFlags) -> obx_err {
+        obx_qb_order(self.obx_query_builder, property_id, flags)
+    }
+
+    pub(crate) unsafe fn link_property(
+        &self,
+        property_id: obx_schema_id,
+    ) -> *mut OBX_query_builder {
+        obx_qb_link_property(self.obx_query_builder, property_id)
+    }
+
+    pub(crate) unsafe fn backlink_property(
+        &self,
+        source_entity_id: obx_schema_id,
+        source_property_id: obx_schema_id,
+    ) -> *mut OBX_query_builder {
+        unsafe {
+            obx_qb_backlink_property(self.obx_query_builder, source_entity_id, source_property_id)
+        }
+    }
+
+    pub(crate) unsafe fn link_standalone(
+        &self,
+        relation_id: obx_schema_id,
+    ) -> *mut OBX_query_builder {
+        obx_qb_link_standalone(self.obx_query_builder, relation_id)
+    }
+
+    pub(crate) unsafe fn backlink_standalone(
+        &self,
+        relation_id: obx_schema_id,
+    ) -> *mut OBX_query_builder {
+        obx_qb_backlink_standalone(self.obx_query_builder, relation_id)
+    }
+
+    pub(crate) unsafe fn link_time(
+        &self,
+        linked_entity_id: obx_schema_id,
+        begin_property_id: obx_schema_id,
+        end_property_id: obx_schema_id,
+    ) -> *mut OBX_query_builder {
+        unsafe {
+            obx_qb_link_time(
+                self.obx_query_builder,
+                linked_entity_id,
+                begin_property_id,
+                end_property_id,
+            )
+        }
+    }
+}
---
+++ src/query/query.rs
@@ -0,0 +1,399 @@
+// TODO implement Drop on QueryProperty etc.
+// TODO whichever holds a C ptr, that has a free/close C fn
+//
+// Reminder:
+// Expression in dart: box.query(i.greaterThan(0)).build().property(pq);
+// box.query -> QueryBuilder
+// i -> QueryProperty (QP)
+// i.greaterThan(0) -> Condition
+// ..build() -> Query
+// ..property(j) -> PropertyQuery (PQ) PQ vs QP are confusing as hell, I named it, mea culpa
+// j -> QP (like i)
+//
+// Traits to reuse: https://doc.rust-lang.org/std/ops/index.html
+// Ops: https://doc.rust-lang.org/book/appendix-02-operators.html
+/*
+enum _ConditionOp {
+  isNull, // TODO only feasible when Option<OB_Rust_Primitive> is introduced
+  notNull, // TODO only feasible when Option<OB_Rust_Primitive> is introduced
+  eq, // std::cmp::PartialEq, eq
+  notEq, // std::ops::PartialEq, ne
+  contains,
+  containsElement,
+  startsWith,
+  endsWith,
+  gt, // std::cmp::PartialOrd, gt
+  greaterOrEq, // std::cmp::PartialOrd, ge
+  lt, // std::cmp::PartialOrd, lt
+  lessOrEq, // std::cmp::PartialOrd, le
+  oneOf,
+  notOneOf,
+  between,
+}
+
+// TODO even better, check predicates: https://docs.rs/predicates/2.1.5/predicates/index.html
+// e.g. box.query(qp)
+
+// For lack of variadic args on .query(), use query(vec!(condition...));
+*/
+
+// TODO write macro for boilerplate: fn obx_query_something(...) -> obx_err, rewrite to call,
+// TODO also error check before chaining the next call
+// TODO depending on property type, allow only certain calls at compile time?
+// TODO compile time determined extension blanket traits?
+
+use std::ptr;
+
+use crate::c::*;
+use crate::error;
+use crate::util::MutConstVoidPtr;
+use crate::util::PtrConstChar;
+
+impl Drop for Query {
+    fn drop(&mut self) {
+        if !self.obx_query.is_null() {
+            // always close regardless, no flags to set, no potential double frees
+            self.close();
+            self.obx_query = ptr::null_mut();
+        }
+
+        if let Some(err) = &self.error {
+            eprintln!("Error: async: {err}");
+        }
+    }
+}
+
+pub struct Query {
+    error: Option<error::Error>,
+    pub(crate) obx_query: *mut OBX_query,
+}
+
+impl Query {
+    pub(crate) fn from_query_builder(builder: *mut OBX_query_builder) -> Self {
+        unsafe {
+            Self {
+                obx_query: obx_query(builder),
+                error: None,
+            }
+        }
+    }
+
+    fn close(&mut self) -> obx_err {
+        unsafe { obx_query_close(self.obx_query) }
+    }
+
+    // TODO potential double frees here?
+    // No Clone trait here, because that implies Copy,
+    // which prevents using Drop
+    fn clone(&self) -> Query {
+        unsafe {
+            Query {
+                obx_query: obx_query_clone(self.obx_query),
+                error: None,
+            }
+        }
+    }
+
+    pub(crate) unsafe fn offset(&mut self, offset: usize) -> obx_err {
+        obx_query_offset(self.obx_query, offset)
+    }
+
+    pub(crate) unsafe fn offset_limit(&mut self, offset: usize, limit: usize) -> obx_err {
+        obx_query_offset_limit(self.obx_query, offset, limit)
+    }
+
+    pub(crate) unsafe fn limit(&mut self, limit: usize) -> obx_err {
+        obx_query_limit(self.obx_query, limit)
+    }
+
+    pub(crate) unsafe fn find(&mut self) -> *mut OBX_bytes_array {
+        obx_query_find(self.obx_query)
+    }
+
+    pub(crate) unsafe fn find_first(
+        &mut self,
+        data: MutConstVoidPtr,
+        size: *mut usize,
+    ) -> obx_err {
+        obx_query_find_first(self.obx_query, data, size)
+    }
+
+    pub(crate) unsafe fn find_unique(
+        &mut self,
+        data: MutConstVoidPtr,
+        size: *mut usize,
+    ) -> obx_err {
+        obx_query_find_unique(self.obx_query, data, size)
+    }
+
+    // TODO pass a closure to this in the pub fn impl
+    pub(crate) unsafe fn visit(
+        &mut self,
+        visitor: obx_data_visitor,
+        user_data: *mut ::std::os::raw::c_void,
+    ) -> obx_err {
+        obx_query_visit(self.obx_query, visitor, user_data)
+    }
+
+    pub(crate) unsafe fn find_ids(&mut self) -> *mut OBX_id_array {
+        obx_query_find_ids(self.obx_query)
+    }
+
+    pub(crate) unsafe fn count(&mut self, out_count: *mut u64) -> obx_err {
+        obx_query_count(self.obx_query, out_count)
+    }
+
+    pub(crate) unsafe fn remove(&mut self, out_count: *mut u64) -> obx_err {
+        obx_query_remove(self.obx_query, out_count)
+    }
+
+    /// For testing and debugging
+    pub unsafe fn describe(&mut self) -> PtrConstChar {
+        obx_query_describe(self.obx_query)
+    }
+
+    /// For testing and debugging
+    pub unsafe fn describe_params(&mut self) -> PtrConstChar {
+        obx_query_describe_params(self.obx_query)
+    }
+
+    pub(crate) unsafe fn cursor_visit(
+        &mut self,
+        cursor: &mut OBX_cursor,
+        visitor: obx_data_visitor,
+        user_data: *mut ::std::os::raw::c_void,
+    ) -> obx_err {
+        obx_query_cursor_visit(self.obx_query, cursor, visitor, user_data)
+    }
+
+    pub(crate) unsafe fn cursor_find(&mut self, cursor: &mut OBX_cursor) -> *mut OBX_bytes_array {
+        obx_query_cursor_find(self.obx_query, cursor)
+    }
+
+    pub(crate) unsafe fn cursor_find_ids(&mut self, cursor: &mut OBX_cursor) -> *mut OBX_id_array {
+        obx_query_cursor_find_ids(self.obx_query, cursor)
+    }
+
+    pub(crate) unsafe fn cursor_count(
+        &mut self,
+        cursor: &mut OBX_cursor,
+        out_count: *mut u64,
+    ) -> obx_err {
+        obx_query_cursor_count(self.obx_query, cursor, out_count)
+    }
+
+    pub(crate) unsafe fn cursor_remove(
+        &mut self,
+        cursor: &mut OBX_cursor,
+        out_count: *mut u64,
+    ) -> obx_err {
+        obx_query_cursor_remove(self.obx_query, cursor, out_count)
+    }
+
+    pub(crate) unsafe fn param_string(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+    ) -> obx_err {
+        obx_query_param_string(self.obx_query, entity_id, property_id, value)
+    }
+
+    // For strings
+    pub(crate) unsafe fn param_2strings(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value: PtrConstChar,
+        value2: PtrConstChar,
+    ) -> obx_err {
+        obx_query_param_2strings(self.obx_query, entity_id, property_id, value, value2)
+    }
+
+    pub(crate) unsafe fn param_strings(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        values: *const PtrConstChar,
+        count: usize,
+    ) -> obx_err {
+        obx_query_param_strings(self.obx_query, entity_id, property_id, values, count)
+    }
+
+    // For ints
+    pub(crate) unsafe fn param_int(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value: i64,
+    ) -> obx_err {
+        obx_query_param_int(self.obx_query, entity_id, property_id, value)
+    }
+
+    pub(crate) unsafe fn param_2ints(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value_a: i64,
+        value_b: i64,
+    ) -> obx_err {
+        obx_query_param_2ints(
+            self.obx_query,
+            entity_id,
+            property_id,
+            value_a,
+            value_b,
+        )
+    }
+
+    pub(crate) unsafe fn param_int64s(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        values: *const i64,
+        count: usize,
+    ) -> obx_err {
+        obx_query_param_int64s(self.obx_query, entity_id, property_id, values, count)
+    }
+
+    pub(crate) unsafe fn param_int32s(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        values: *const i32,
+        count: usize,
+    ) -> obx_err {
+        obx_query_param_int32s(self.obx_query, entity_id, property_id, values, count)
+    }
+
+    // For doubles
+    pub(crate) unsafe fn param_double(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value: f64,
+    ) -> obx_err {
+        obx_query_param_double(self.obx_query, entity_id, property_id, value)
+    }
+
+    pub(crate) unsafe fn param_2doubles(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value_a: f64,
+        value_b: f64,
+    ) -> obx_err {
+        obx_query_param_2doubles(
+            self.obx_query,
+            entity_id,
+            property_id,
+            value_a,
+            value_b,
+        )
+    }
+
+    // For bytes
+    pub(crate) unsafe fn param_bytes(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_err {
+        obx_query_param_bytes(self.obx_query, entity_id, property_id, value, size)
+    }
+
+    pub(crate) unsafe fn param_get_type_size(
+        &mut self,
+        entity_id: obx_schema_id,
+        property_id: obx_schema_id,
+    ) -> usize {
+        obx_query_param_get_type_size(self.obx_query, entity_id, property_id)
+    }
+
+    // For aliases
+    pub(crate) unsafe fn param_alias_string(
+        &mut self,
+        alias: PtrConstChar,
+        value: PtrConstChar,
+    ) -> obx_err {
+        obx_query_param_alias_string(self.obx_query, alias, value)
+    }
+
+    pub(crate) unsafe fn param_alias_strings(
+        &mut self,
+        alias: PtrConstChar,
+        values: *const PtrConstChar,
+        count: usize,
+    ) -> obx_err {
+        obx_query_param_alias_strings(self.obx_query, alias, values, count)
+    }
+
+    pub(crate) unsafe fn param_alias_int(
+        &mut self,
+        alias: PtrConstChar,
+        value: i64,
+    ) -> obx_err {
+        obx_query_param_alias_int(self.obx_query, alias, value)
+    }
+
+    pub(crate) unsafe fn param_alias_2ints(
+        &mut self,
+        alias: PtrConstChar,
+        value_a: i64,
+        value_b: i64,
+    ) -> obx_err {
+        obx_query_param_alias_2ints(self.obx_query, alias, value_a, value_b)
+    }
+
+    pub(crate) unsafe fn param_alias_int64s(
+        &mut self,
+        alias: PtrConstChar,
+        values: *const i64,
+        count: usize,
+    ) -> obx_err {
+        obx_query_param_alias_int64s(self.obx_query, alias, values, count)
+    }
+
+    pub(crate) unsafe fn param_alias_int32s(
+        &mut self,
+        alias: PtrConstChar,
+        values: *const i32,
+        count: usize,
+    ) -> obx_err {
+        obx_query_param_alias_int32s(self.obx_query, alias, values, count)
+    }
+
+    pub(crate) unsafe fn param_alias_double(
+        &mut self,
+        alias: PtrConstChar,
+        value: f64,
+    ) -> obx_err {
+        obx_query_param_alias_double(self.obx_query, alias, value)
+    }
+
+    pub(crate) unsafe fn param_alias_2doubles(
+        &mut self,
+        alias: PtrConstChar,
+        value_a: f64,
+        value_b: f64,
+    ) -> obx_err {
+        obx_query_param_alias_2doubles(self.obx_query, alias, value_a, value_b)
+    }
+
+    pub(crate) unsafe fn param_alias_bytes(
+        &mut self,
+        alias: PtrConstChar,
+        value: *const ::std::os::raw::c_void,
+        size: usize,
+    ) -> obx_err {
+        obx_query_param_alias_bytes(self.obx_query, alias, value, size)
+    }
+
+    pub(crate) unsafe fn param_alias_get_type_size(
+        &mut self,
+        alias: PtrConstChar,
+    ) -> usize {
+        obx_query_param_alias_get_type_size(self.obx_query, alias)
+    }
+}
