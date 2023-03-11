fe8722131dca6d4a4339eba11e920c3814057754
Jasm Sison <jasm.sison@gmail.com>
2023-03-11 03:01:12 +0100

    get rid of macro warning

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,7 +1,8 @@
-/// The entities are prefixed with self::Entity
-/// so that the generated file can be
-/// included relative to the same module as 
-/// the actual entity structs
+// The entities are prefixed with self::Entity
+// so that the generated file can be
+// included relative to the same module as 
+// the actual entity structs
+
 include!("./entities.rs");
 include!("./objectbox_gen.rs");
 
